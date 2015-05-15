bdcb83154d13c77d1e3da4ea3b185b343ef5a61a
manageryzy <manageryzy@live.cn>
2015-05-15 16:59:15 +0800

    把minXMLPrase的框架扔上来啦 Signed-off-by: manageryzy <manageryzy@live.cn>

--- lineCycle/xml/minXml.cpp
+++ lineCycle/xml/minXml.cpp
@@ -3,8 +3,49 @@
 //极小化的基于状态机的xml解析
 //
 
+namespace minXMLPrase
+{
+	FILE * fp;
+	WCHAR charBuf;
+	bool praseXML(WCHAR *);
+	inline void onCharRead();
+
+	inline void onCharRead()
+	{
+
+	}
+
+	bool praseXML(WCHAR * fileName)
+	{
+		_wfopen_s(&fp, fileName, L"rb");
+		if (fp == NULL)
+		{
+			return false;
+		}
+
+		while(1)
+		{
+			charBuf = fgetwc(fp);
+			if (charBuf == EOF)
+				break;
+
+			onCharRead();
+		}
+		
+		fclose(fp);
+		
+		return true;
+	}
+}
+
 
 bool minXMLPrase()
 {
-	return true;
+	using namespace minXMLPrase;
+
+	isXMLBusy = true;
+	bool res = praseXML(strFile);
+	isXMLBusy = false;
+
+	return res;
 }
