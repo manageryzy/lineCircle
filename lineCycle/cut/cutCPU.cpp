e308883c921b401cdace6e08ab4fcabd4227c292
manageryzy <manageryzy@live.cn>
2015-05-23 21:13:40 +0800

    直线裁剪错误修复 Signed-off-by: manageryzy <manageryzy@live.cn>

--- lineCycle/cut/cutCPU.cpp
+++ lineCycle/cut/cutCPU.cpp
@@ -11,7 +11,7 @@ namespace cpuCUT{
 	const double eps = 1e-8;
 
 
-	float mult(Point a, Point b, Point c){
+	inline float mult(Point a, Point b, Point c){
 		return (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
 	}
 
@@ -71,7 +71,7 @@ namespace cpuCUT{
 	//	return counter & 1;
 	//}
 
-	int isPointIn(const Point &p)
+	inline int isPointIn(const Point &p)
 	{
 		if (gra[(int)p.y][(int)p.x] != 0)
 			return true;
@@ -84,7 +84,7 @@ namespace cpuCUT{
 
 	//求直线 l1； a1*x + b1*y + c1 = 0 与直线 l2： a2*x + b2*y + c2 = 0 的交点
 	//a，b为直线 l1 上的两点， c , d为直线 l2 上的两点
-	Point * intersection(float ax, float ay, float bx, float by, float cx, float cy, float dx, float dy)
+	inline Point * intersection(float ax, float ay, float bx, float by, float cx, float cy, float dx, float dy)
 	{
 		float a1 = by - ay;
 		float b1 = ax - bx;
@@ -98,6 +98,7 @@ namespace cpuCUT{
 
 		if (x < min(ax,bx) || x > max(ax,bx) || x < min(cx,dx) || x > max(cx,dx)) return NULL; //线段没有交点
 		float y = (c1 * a2 - c2 * a1) / dlt; //交点纵坐标
+		if (y < min(ay, by) || y > max(ay, by) || y < min(cy, dy) || y > max(cy, dy)) return NULL; //线段没有交点
 		//if ((int)x == (int)cx && (int)y == (int)cy) return NULL;
 		Point * res = (Point *)mempool->Alloc(sizeof(Point));
 		res->x = x, res->y = y;
