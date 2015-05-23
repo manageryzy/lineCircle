187213f45f0bdc09a87724b02d8a6dce4c2fbfd5
manageryzy <manageryzy@live.cn>
2015-05-23 20:29:22 +0800

    失败的多线程版本……算了下次弄只有一个裁剪线程的好了…… Signed-off-by: manageryzy <manageryzy@live.cn>

--- lineCycle/cut/cutCPU.cpp
+++ lineCycle/cut/cutCPU.cpp
@@ -6,6 +6,8 @@ namespace cpuCUT{
 
 	int gra[768][1366];
 
+	HANDLE * events;
+
 	const double eps = 1e-8;
 
 
@@ -146,6 +148,200 @@ namespace cpuCUT{
 		*res2 = alp;    //第二个点
 		return true;
 	}
+
+	DWORD WINAPI cutLineWorker(LPVOID lpParam)
+	{
+		int workerID = (int)lpParam;
+		int workerInterval = lineList.size() / SETTING_DRAW_THREAD;
+
+		vector < Point *> lineCuttingPointList;
+
+		if (workerID == 0)
+		{
+			for (unsigned int i = 0; i < workerInterval + lineList.size() % SETTING_DRAW_THREAD; i++)
+			{
+				Line * l = lineList.at(i);
+				int size;
+
+				//清除掉上次的垃圾
+				size = lineCuttingPointList.size();
+				for (int i = 0; i < size; i++)
+				{
+					mempool->Free(lineCuttingPointList.at(i));
+				}
+				lineCuttingPointList.clear();
+
+				Point * pth = (Point *)mempool->Alloc(sizeof(Point));
+				pth->x = l->x1;
+				pth->y = l->y1;
+				lineCuttingPointList.push_back(pth);
+
+				//建立交点数组
+				size = polygonList.size();
+				for (int j = 0; j < size; j++)
+				{
+					Point * pt1 = polygonList.at(j);
+					Point * pt2;
+					if (j == size - 1)
+						pt2 = polygonList.at(0);
+					else
+						pt2 = polygonList.at(j + 1);
+
+					Point * res = intersection(l->x1, l->y1, l->x2, l->y2, pt1->x, pt1->y, pt2->x, pt2->y);
+					if (res != NULL)
+					{
+						lineCuttingPointList.push_back(res);
+					}
+				}
+
+				Point * pte = (Point *)mempool->Alloc(sizeof(Point));
+				pte->x = l->x2;
+				pte->y = l->y2;
+				lineCuttingPointList.push_back(pte);
+
+				if (lineCuttingPointList.at(0)->x != lineCuttingPointList.at(1)->x)
+					sort(lineCuttingPointList.begin(), lineCuttingPointList.end(), cmpx);
+				else
+					sort(lineCuttingPointList.begin(), lineCuttingPointList.end(), cmpy);
+
+				size = lineCuttingPointList.size();
+				Point tmpPoint;
+				for (int j = 0; j < size - 1; j++)
+				{
+					Point * pt1 = lineCuttingPointList.at(j);
+					Point * pt2 = lineCuttingPointList.at(j + 1);
+					tmpPoint.x = (pt1->x + pt2->x) / 2;
+					tmpPoint.y = (pt1->y + pt2->y) / 2;
+					if (isPointIn(tmpPoint))
+					{
+						Line * l = (Line *)mempool->Alloc(sizeof(Line));
+						l->x1 = pt1->x;
+						l->y1 = pt1->y;
+						l->x2 = pt2->x;
+						l->y2 = pt2->y;
+						cutLineList.push_back(l);
+					}
+				}
+			}
+		}
+		else
+		{
+			for (unsigned int i = workerInterval*workerID + lineList.size() % SETTING_DRAW_THREAD; i < workerInterval*(workerID + 1) + circleList.size() % SETTING_DRAW_THREAD; i++)
+			{
+				Line * l = lineList.at(i);
+				int size;
+
+				//清除掉上次的垃圾
+				size = lineCuttingPointList.size();
+				for (int i = 0; i < size; i++)
+				{
+					mempool->Free(lineCuttingPointList.at(i));
+				}
+				lineCuttingPointList.clear();
+
+				Point * pth = (Point *)mempool->Alloc(sizeof(Point));
+				pth->x = l->x1;
+				pth->y = l->y1;
+				lineCuttingPointList.push_back(pth);
+
+				//建立交点数组
+				size = polygonList.size();
+				for (int j = 0; j < size; j++)
+				{
+					Point * pt1 = polygonList.at(j);
+					Point * pt2;
+					if (j == size - 1)
+						pt2 = polygonList.at(0);
+					else
+						pt2 = polygonList.at(j + 1);
+
+					Point * res = intersection(l->x1, l->y1, l->x2, l->y2, pt1->x, pt1->y, pt2->x, pt2->y);
+					if (res != NULL)
+					{
+						lineCuttingPointList.push_back(res);
+					}
+				}
+
+				Point * pte = (Point *)mempool->Alloc(sizeof(Point));
+				pte->x = l->x2;
+				pte->y = l->y2;
+				lineCuttingPointList.push_back(pte);
+
+				if (lineCuttingPointList.at(0)->x != lineCuttingPointList.at(1)->x)
+					sort(lineCuttingPointList.begin(), lineCuttingPointList.end(), cmpx);
+				else
+					sort(lineCuttingPointList.begin(), lineCuttingPointList.end(), cmpy);
+
+				size = lineCuttingPointList.size();
+				Point tmpPoint;
+				for (int j = 0; j < size - 1; j++)
+				{
+					Point * pt1 = lineCuttingPointList.at(j);
+					Point * pt2 = lineCuttingPointList.at(j + 1);
+					tmpPoint.x = (pt1->x + pt2->x) / 2;
+					tmpPoint.y = (pt1->y + pt2->y) / 2;
+					if (isPointIn(tmpPoint))
+					{
+						Line * l = (Line *)mempool->Alloc(sizeof(Line));
+						l->x1 = pt1->x;
+						l->y1 = pt1->y;
+						l->x2 = pt2->x;
+						l->y2 = pt2->y;
+						cutLineList.push_back(l);
+					}
+				}
+			}
+		}
+
+		SetEvent(events[workerID]);
+		return 0;
+	}
+
+
+	//切割启动线程
+	DWORD WINAPI cuttingThread(LPVOID lpParam)
+	{
+
+		if (polygonList.size() < 3)
+		{
+			MessageBox(theHWND, L"边界内容过少", L"错误", 0);
+			return 0;
+		}
+
+		if (polygonList.size() > MAX_POLYGON_SIZE)
+		{
+			MessageBox(theHWND, L"边界内容过多，请修改参数重新编译", L"错误", 0);
+			return 0;
+		}
+
+		events = new HANDLE[SETTING_CUTTING_THREAD];
+		for (int i = 0; i < SETTING_CUTTING_THREAD; i++)
+			events[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+
+		//多线程绘制直线
+		for (int i = 0; i < SETTING_DRAW_THREAD; i++)
+		{
+			HANDLE hThread;
+
+			hThread = CreateThread(NULL, 0, cutLineWorker, (LPVOID)i, 0, NULL);
+			if (hThread == NULL)
+			{
+				MessageBox(theHWND, L"线程创建失败", L"错误", 0);
+				PostMessage(theHWND, WM_DESTROY, 0, 0);
+			}
+
+			CloseHandle(hThread);
+		}
+
+		WaitForMultipleObjects(SETTING_DRAW_THREAD, events, true, INFINITE);
+
+		for (int i = 0; i < SETTING_DRAW_THREAD; i++)
+			CloseHandle(events[i]);
+
+		delete[] events;
+
+		return 0;
+	}
 }
 
 //非阻塞多线程切割图形
@@ -153,78 +349,16 @@ void doCPUCut()
 {
 	using namespace cpuCUT;
 
-	if (polygonList.size() < 3)
+	HANDLE hThread;
+
+	hThread = CreateThread(NULL, 0, cuttingThread, 0, 0, NULL);
+	if (hThread == NULL)
 	{
-		MessageBox(theHWND, L"边界内容过少", L"错误", 0);
-		return;
+		MessageBox(theHWND, L"线程创建失败", L"错误", 0);
+		PostMessage(theHWND, WM_DESTROY, 0, 0);
 	}
 
-	vector < Point *> lineCuttingPointList;
-	for (unsigned int i = 0; i < lineList.size(); i++)
-	{
-		Line * l = lineList.at(i);
-		int size;
-
-		//清除掉上次的垃圾
-		size = lineCuttingPointList.size();
-		for (int i = 0; i < size; i++)
-		{
-			mempool->Free(lineCuttingPointList.at(i));
-		}
-		lineCuttingPointList.clear();
-
-		Point * pth = (Point *)mempool->Alloc(sizeof(Point));
-		pth->x = l->x1;
-		pth->y = l->y1;
-		lineCuttingPointList.push_back(pth);
-
-		//建立交点数组
-		size = polygonList.size();
-		for (int j = 0; j < size; j++)
-		{
-			Point * pt1 = polygonList.at(j);
-			Point * pt2;
-			if (j == size - 1)
-				pt2 = polygonList.at(0);
-			else
-				pt2 = polygonList.at(j + 1);
-
-			Point * res = intersection(l->x1, l->y1, l->x2, l->y2, pt1->x, pt1->y, pt2->x, pt2->y);
-			if (res != NULL)
-			{
-				lineCuttingPointList.push_back(res);
-			}
-		}
-
-		Point * pte = (Point *)mempool->Alloc(sizeof(Point));
-		pte->x = l->x2;
-		pte->y = l->y2;
-		lineCuttingPointList.push_back(pte);
-
-		if (lineCuttingPointList.at(0)->x != lineCuttingPointList.at(1)->x)
-			sort(lineCuttingPointList.begin(), lineCuttingPointList.end(),cmpx);
-		else
-			sort(lineCuttingPointList.begin(), lineCuttingPointList.end(), cmpy);
-
-		size = lineCuttingPointList.size();
-		Point tmpPoint;
-		for (int j = 0; j < size - 1; j++)
-		{
-			Point * pt1 = lineCuttingPointList.at(j);
-			Point * pt2 = lineCuttingPointList.at(j + 1);
-			tmpPoint.x = (pt1->x + pt2->x) / 2;
-			tmpPoint.y = (pt1->y + pt2->y) / 2;
-			if (isPointIn(tmpPoint))
-			{
-				Line * l = (Line *)mempool->Alloc(sizeof(Line));
-				l->x1 = pt1->x;
-				l->y1 = pt1->y;
-				l->x2 = pt2->x;
-				l->y2 = pt2->y;
-				cutLineList.push_back(l);
-			}
-		}
-	}
+	CloseHandle(hThread);
 }
 
 bool initGra()
