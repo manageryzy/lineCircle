0f21f0d61e23aba7857b97961ff6b9f32a4cf0ee
manageryzy <manageryzy@live.cn>
2015-05-23 22:12:15 +0800

    莫名其妙的开不了优化，算了……那就裁剪这里不开优化好了（满了一半QAQ Signed-off-by: manageryzy <manageryzy@live.cn>

--- lineCycle/cut/cutCPU.cpp
+++ lineCycle/cut/cutCPU.cpp
@@ -1,4 +1,5 @@
 #include  "../stdafx.h"
+#pragma optimize( "", off )
 
 namespace cpuCUT{
 
@@ -95,10 +96,9 @@ namespace cpuCUT{
 		float dlt = a1 * b2 - a2 * b1;
 		if (dlt == 0) return NULL;    //线段平行
 		float x = (c2 * b1 - c1 * b2) / dlt;
-
-		if (x < min(ax,bx) || x > max(ax,bx) || x < min(cx,dx) || x > max(cx,dx)) return NULL; //线段没有交点
 		float y = (c1 * a2 - c2 * a1) / dlt; //交点纵坐标
-		if (y < min(ay, by) || y > max(ay, by) || y < min(cy, dy) || y > max(cy, dy)) return NULL; //线段没有交点
+
+		if (x < min(ax, bx) || x > max(ax, bx) || x < min(cx, dx) || x > max(cx, dx) || y < min(ay, by) || y > max(ay, by) || y < min(cy, dy) || y > max(cy, dy)) return NULL; //线段没有交点
 		//if ((int)x == (int)cx && (int)y == (int)cy) return NULL;
 		Point * res = (Point *)mempool->Alloc(sizeof(Point));
 		res->x = x, res->y = y;
